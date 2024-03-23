f2e0424013a56d80bdac9924bcd32192cafc43a3
Achmad Sufyan Aziz <achmadsufyan98@gmail.com>
2024-03-23 23:07:25 +0700

    feat: init commit & task 1 (eid-card)

---
+++ src/components/EidCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const EidCard = () => {
+  return (
+    <div className="eid-card-container">
+      <div className="eid-card-diamond-container">
+        <div className="eid-card-diamond-section">
+          <div className="eid-card-diamond" />
+        </div>
+        <div className="eid-card-diamond-section">
+          <div className="eid-card-diamond secondary" />
+          <div className="eid-card-diamond secondary" />
+        </div>
+        <div className="eid-card-diamond-section">
+          <div className="eid-card-diamond" />
+          <div className="eid-card-diamond" />
+          <div className="eid-card-diamond" />
+        </div>
+        <div className="eid-card-diamond-section">
+          <div className="eid-card-diamond secondary" />
+          <div className="eid-card-diamond secondary" />
+        </div>
+        <div className="eid-card-diamond-section">
+          <div className="eid-card-diamond" />
+        </div>
+      </div>
+      <div className="eid-card-text">
+        <p>.: Selamat Hari Raya Idul Fitri :.</p>
+        <p>Mohon maaf lahir dan batin &hearts;</p>
+      </div>
+    </div>
+  );
+};
+
+export default EidCard;
